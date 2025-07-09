887ad20dab737c184fbf8599d18de38e9ecbcf45
Gustavo Orelio <oreliogustavo@gmail.com>
2025-07-08 23:25:20 -0300

    feat: adiciona configuração do Next.js e atualiza dependências

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Configuração para o PDFKit
+    if (isServer) {
+      config.module.rules.push({
+        test: /\.(afm|ttf|woff|woff2)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "static/[hash][ext]",
+        },
+      });
+    }
+
+    return config;
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["pdfkit"],
+  },
+};
+
+module.exports = nextConfig;
