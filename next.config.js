0110f99dffd9538013fc98d633519b32bc6e50ef
Gustavo Orelio <oreliogustavo@gmail.com>
2025-07-14 20:33:58 -0300

    chore: atualiza configuração do Next.js

--- next.config.js
+++ next.config.js
@@ -14,9 +14,7 @@ const nextConfig = {
 
     return config;
   },
-  experimental: {
-    serverComponentsExternalPackages: ["pdfkit"],
-  },
+  serverComponentsExternalPackages: ["pdfkit"],
 };
 
 module.exports = nextConfig;
